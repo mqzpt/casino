70ce13c70d3a25a521250f69234e63f8baea44b5
Matthew Athanasopoulos <matthew.athanasopoulos@gmail.com>
2025-04-21 00:57:48 -0700

    feat: create initial slot logic in cli

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func getName() string {
@@ -27,9 +28,112 @@ func getBet(balance uint) uint {
 			break
 		}
 	}
+	return bet
+}
+
+func generateSymbolArray(symbols map[string]uint) []string {
+	symbolArr := []string{}
+	for symbol, count := range symbols {
+		for i := uint(0); i < count; i++ {
+			symbolArr = append(symbolArr, symbol)
+		}
+	}
+	return symbolArr
+}
+
+func getRandomNumber(min int, max int) int {
+	randomNumber := rand.Intn(max-min+1) + min
+	return randomNumber
+}
+
+func getSpin(reel []string, rows int, cols int) [][]string {
+	result := [][]string{}
+
+	for i := 0; i < rows; i++ {
+		result = append(result, []string{})
+	}
+
+	for col := 0; col < cols; col++ {
+		selected := map[int]bool{}
+		for row := 0; row < rows; row++ {
+			for true {
+				randomIndex := getRandomNumber(0, len(reel)-1)
+				_, exists := selected[randomIndex]
+				if !exists {
+					selected[randomIndex] = true
+					result[row] = append(result[row], reel[randomIndex])
+					break
+				}
+			}
+		}
+	}
+	return result
+}
+
+func printSpin(spin [][]string) {
+	fmt.Println()
+	for _, row := range spin {
+		for j, symbol := range row {
+			fmt.Print(symbol)
+			if j != len(row)-1 {
+				fmt.Printf(" | ")
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func checkWin(spin [][]string, multipliers map[string]uint) []uint {
+	lines := []uint{}
+
+	for _, row := range spin {
+		win := true
+		checkSymbol := row[0]
+		for _, symbol := range row[1:] {
+			if checkSymbol != symbol {
+				win = false
+				break
+			}
+		}
+		if win {
+			lines = append(lines, multipliers[checkSymbol])
+		} else {
+			lines = append(lines, 0)
+		}
+	}
+	return lines
 }
 
 func main() {
-	balance := 200
+	symbols := map[string]uint{
+		"A": 4,
+		"B": 7,
+		"C": 12,
+		"D": 20,
+	}
+
+	multipliers := map[string]uint{
+		"A": 20,
+		"B": 10,
+		"C": 5,
+		"D": 2,
+	}
+
+	symbolArr := generateSymbolArray(symbols)
+	balance := uint(200)
 	getName()
+
+	for balance > 0 {
+		bet := getBet(balance)
+		if bet == 0 {
+			break
+		}
+		balance -= bet
+		spin := getSpin(symbolArr, 3, 3)
+		printSpin(spin)
+		winningLines := checkWin(spin, multipliers)
+		fmt.Println(winningLines)
+	}
+	fmt.Printf("You left with $%d\n.", balance)
 }
